03faeeab6b4e851bb2505bd1ed2547acd970011b
mart vettner <martvettner@gmail.com>
2020-10-08 11:25:27 +0300

    Add project files.

---
+++ Params/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParasSum(5, 5);
+        }
+
+        public static void ParasSum(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+        public static void StringFunction()
+        {
+            Console.WriteLine("enter string");
+            string functionString = Console.ReadLine();
+            Console.WriteLine(functionString);
+        }
+            
+
+
+    }
+}
---
+++ Week6Functions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week6Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HelloUSer();
+            Num();
+        }
+
+        public static void HelloUSer()
+        {
+            Console.WriteLine("enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"hello, {userName}");
+        }
+        public static void Num()
+        {
+            Console.WriteLine("enter number 1");
+            int Num1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter number 2");
+            int Num2 = Int32.Parse(Console.ReadLine());
+
+            int sum = Num1 + Num2;
+            Console.WriteLine(sum);
+            
+        }
+
+    }
+}
